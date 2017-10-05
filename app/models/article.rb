1ed094817d5f47b5fb2e53eb5ecea1be7cbc7166
Paul Filipow <paul@filipow.ca>
2017-10-04 20:42:11 -0700

    comment views

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,10 @@
 class Article < ApplicationRecord
   belongs_to :blog
   belongs_to :author, class_name: "User"
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   def to_s
     title
   end
-  
+
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,8 +1,13 @@
 class Blog < ApplicationRecord
-  belongs_to :owner, class_name: "User"   #, foreign_key: :owner_id
+  belongs_to :owner, class_name: "User" 
   has_many :articles
-
+  has_many :comments, through: :articles
+  
   def to_s
     title
+  end
+
+  def user_is_owner(user)
+    user == owner
   end
 end
